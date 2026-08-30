82bcac8cfdb937794e489925529c9cfb4d353e59
Ani <205335742+Ani0o@users.noreply.github.com>
2026-08-30 21:01:20 +0530

    Add function to display the books on the page

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.haveRead = read;
+    this.read = read;
     this.info = function() {
-        return `The ${title} by ${author}, ${pages} pages, ${(this.haveRead === "true") ? "have read" : "not read yet"}`;
+        return `The ${title} by ${author}, ${pages} pages, ${(this.read === "true") ? "have read" : "not read yet"}`;
     }
 }
 
@@ -18,3 +18,40 @@ function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
+
+// Manually added books to test display
+addBookToLibrary("Bleach", "Kubo", "95", "true");
+addBookToLibrary("Demon Slayer", "Koyoharu Gotouge", "55", "true");
+addBookToLibrary("Tomodachi Game", "Yuki Sato", "45", "false");
+
+function display() {
+    const container = document.querySelector(".book-container");
+
+    myLibrary.forEach(book => {
+        const cardElement = document.createElement("div");
+        const titleElement = document.createElement("p");
+        const authorElement = document.createElement("p");
+        const pagesElement = document.createElement("p");
+        const readElement = document.createElement("p");
+
+        cardElement.classList.add("card");
+        titleElement.classList.add("title");
+        authorElement.classList.add("author");
+        pagesElement.classList.add("pages");
+        readElement.classList.add("read");
+
+        titleElement.innerText = book.title;
+        authorElement.innerText = book.author;
+        pagesElement.innerText = book.pages;
+        readElement.innerText = book.read;
+
+        cardElement.appendChild(titleElement);
+        cardElement.appendChild(authorElement);
+        cardElement.appendChild(pagesElement);
+        cardElement.appendChild(readElement);
+
+        container.appendChild(cardElement);
+    });
+}
+
+display();
