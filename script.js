72ea160144d6ecf9f6de4f4a6813b13bd0049f56
Ani <205335742+Ani0o@users.noreply.github.com>
2026-08-30 22:02:08 +0530

    Change the layout of the card

--- script.js
+++ script.js
@@ -23,18 +23,23 @@ function addBookToLibrary(title, author, pages, read) {
 addBookToLibrary("Bleach", "Kubo", "95", "true");
 addBookToLibrary("Demon Slayer", "Koyoharu Gotouge", "55", "true");
 addBookToLibrary("Tomodachi Game", "Yuki Sato", "45", "false");
+addBookToLibrary("Tokyo Ghoul", "Sui Ishida", "88", "true");
 
 function display() {
     const container = document.querySelector(".book-container");
 
     myLibrary.forEach(book => {
         const cardElement = document.createElement("div");
+        const topSectionElement = document.createElement("div");
+        const bottomSectionElement = document.createElement("div"); 
         const titleElement = document.createElement("p");
         const authorElement = document.createElement("p");
         const pagesElement = document.createElement("p");
         const readElement = document.createElement("p");
 
         cardElement.classList.add("card");
+        topSectionElement.classList.add("top-section");
+        bottomSectionElement.classList.add("bottom-section");
         titleElement.classList.add("title");
         authorElement.classList.add("author");
         pagesElement.classList.add("pages");
@@ -42,13 +47,17 @@ function display() {
 
         titleElement.innerText = book.title;
         authorElement.innerText = book.author;
-        pagesElement.innerText = book.pages;
+        pagesElement.innerText = `Pages: ${book.pages}`;
         readElement.innerText = (book.read === "true") ? "Read" : "Not Read";
 
-        cardElement.appendChild(titleElement);
-        cardElement.appendChild(authorElement);
-        cardElement.appendChild(pagesElement);
-        cardElement.appendChild(readElement);
+        topSectionElement.appendChild(titleElement);
+        topSectionElement.appendChild(authorElement);
+
+        bottomSectionElement.appendChild(pagesElement);
+        bottomSectionElement.appendChild(readElement);
+
+        cardElement.appendChild(topSectionElement);
+        cardElement.appendChild(bottomSectionElement);
 
         container.appendChild(cardElement);
     });
